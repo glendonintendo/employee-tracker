76bdc42a78b8006b40b9c0081b7fe7c0b61cabe5
glendo <gleneggers64@gmail.com>
2021-04-24 20:58:16 -0500

    add routes for get all departments and add department

--- routes/departmentRoutes.js
+++ routes/departmentRoutes.js
@@ -1,9 +1,46 @@
 const express = require('express');
 const db = require('../db/connection.js');
+const inputCheck = require('../utils/inputCheck.js');
 const router = express.Router();
 
 // view all departments
+router.get('/departments', (req, res) => {
+    const sql = `SELECT * FROM departments`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
 
 // add department
+router.post('/department', ({ body }, res) => {
+    const errors = inputCheck(body, 'name');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    
+    const sql = `INSERT INTO departments (name) VALUES (?)`;
+    const params = [body.name];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body,
+            changes: result.affectedRows
+        });
+    });
+});
 
 module.exports = router;
