dffa944468ef0fd68d46c19745378ffbc800f12b
glendo <gleneggers64@gmail.com>
2021-04-24 21:11:54 -0500

    add routes for get all employees, add employee, and edit employee role

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,11 +1,77 @@
 const express = require('express');
 const db = require('../db/connection.js');
+const inputCheck = require('../utils/inputCheck.js');
 const router = express.Router();
 
 // view all employees
+router.get('/employees', (req, res) => {
+    const sql = `SELECT * FROM employees`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
 
 // add employee
+router.post('/employee', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'role_id', 'manager_id');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`;
+    const params = [body.first_name, body.last_name, body.role_id, body.manager_id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(500).json({ error: err });
+            return;
+        }
+        console.log(result);
+        res.json({
+            message: 'success',
+            data: body,
+            changes: result.affectedRows
+        });
+    });
+});
 
 // update employee role
+router.put('/employee/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'role_id');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    console.log(req.body);
+    const sql = `UPDATE employees SET role_id = ? WHERE id = ?`;
+    const params = [req.body.role_id, req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Employee not found'
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
 
 module.exports = router;
