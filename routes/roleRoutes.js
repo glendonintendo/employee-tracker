8752bb7ec89f5ca8d609248c5c13d13ba1eece5a
glendo <gleneggers64@gmail.com>
2021-04-24 20:57:10 -0500

    add routes for get all roles and post role

--- routes/roleRoutes.js
+++ routes/roleRoutes.js
@@ -1,9 +1,46 @@
 const express = require('express');
 const db = require('../db/connection.js');
+const inputCheck = require('../utils/inputCheck.js');
 const router = express.Router();
 
 // view all roles
+router.get('/roles', (req, res) => {
+    sql = `SELECT * FROM roles`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
 
 // add role
+router.post('/role', ({ body }, res) => {
+    const errors = inputCheck(body, 'title', 'salary', 'department_id');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)`;
+    const params = [body.title, body.salary, body.department_id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body,
+            changes: result.affectedRows
+        });
+    });
+});
 
 module.exports = router;
