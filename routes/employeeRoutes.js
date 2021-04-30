d3da13b425881443de5f878b35687c61fcc62725
glendo <gleneggers64@gmail.com>
2021-04-30 13:55:22 -0500

    edit get employees route to handle queries for manager_id and department_id

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -5,12 +5,42 @@ const router = express.Router();
 
 // view all employees
 router.get('/employees', (req, res) => {
-    const sql = `
-        SELECT e.id, e.first_name, e.last_name, title, name AS department, salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+    let sql = `
+        SELECT e.id, CONCAT(e.first_name, ' ', e.last_name) AS full_name, title, name AS department, salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
         FROM employees e
         LEFT JOIN roles ON e.role_id = roles.id
         LEFT JOIN departments ON roles.department_id = departments.id
-        LEFT JOIN employees m ON e.manager_id = m.id;
+        LEFT JOIN employees m ON e.manager_id = m.id
+    `;
+
+    if (req.query.manager_id) {
+        sql += `WHERE e.manager_id = ${req.query.manager_id}`;
+    } else if (req.query.department_id) {
+        sql += `WHERE department_id = ${req.query.department_id}`;
+    }
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// view employees who are managers
+router.get('/employees/managers', (req, res) => {
+    const sql = `
+        SELECT id, CONCAT(first_name, ' ', last_name) AS full_name 
+        FROM EMPLOYEES
+        WHERE id IN (
+            SELECT DISTINCT manager_id
+            FROM employees
+            WHERE manager_id IS NOT NULL
+        );
     `;
 
     db.query(sql, (err, rows) => {
@@ -41,7 +71,6 @@ router.post('/employee', ({ body }, res) => {
             res.status(500).json({ error: err });
             return;
         }
-        console.log(result);
         res.json({
             message: 'success',
             data: body,
@@ -62,7 +91,7 @@ router.put('/employee/:id', (req, res) => {
         sql = `UPDATE employees SET manager_id = ? WHERE id = ?`;
         params = [req.body.manager_id, req.params.id];
     }
-    
+
     db.query(sql, params, (err, result) => {
         if (err) {
             res.status(400).json({ error: err.message });
